1d5fc448f756b7386257aa534da5e90ee58e7870
ButterEngine <88644395+ButterEngine@users.noreply.github.com>
2021-10-20 22:57:05 +0700

    Add files via upload

---
+++ Hello SFML/Hello SFML/assets/texture/map/TileSet_Stardew.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="88632 (1)" tilewidth="64" tileheight="64" tilecount="1975" columns="25">
+ <image source="../../../../../../../Users/66612/Downloads/88632 (1).png" width="1600" height="5056"/>
+</tileset>
